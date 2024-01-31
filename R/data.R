808d2527fab98726af723647da56f9556827401f
Margarita Kapustina <129800017+MargoKapustina@users.noreply.github.com>
2024-01-30 17:25:40 -0800

    added files except /data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+###Dataset documentation###
+
+#' Anterodorsal (AD) nucleus of the anterior thalamic nuclei
+#'
+#' Contains subset of Xenium data, including clusters spatially associated
+#'   with the anterodorsal nucleus of the ATN. FOVs included: fov and X1fov,
+#'
+#'
+#'  Format: Seurat Object, with UMAP_1 embeddings computed and stored within.
+#'
+#' Created in-house from Cembrowski Lab Xenium run to serve as example data.
+#'
+#'  To load: data(AD) note: Lazy loading.
+"AD"
